3b2c504b787ae1fe29ce71a5d9c48d9f154305dd
Shayne Brady <hello@shaynebrady.com>
2018-12-02 13:18:37 +1100

    Initial Commit

---
+++ FlexiTable/FlexiTable.h
@@ -0,0 +1,19 @@
+//
+//  FlexiTable.h
+//  FlexiTable
+//
+//  Created by Shayne Brady on 2/12/18.
+//  Copyright © 2018 Shayne Brady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlexiTable.
+FOUNDATION_EXPORT double FlexiTableVersionNumber;
+
+//! Project version string for FlexiTable.
+FOUNDATION_EXPORT const unsigned char FlexiTableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlexiTable/PublicHeader.h>
+
+
